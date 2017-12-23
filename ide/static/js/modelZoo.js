4295ccb835b8f9aab7d7c8b972d088339fb488a3
galiaxy_ <you@example.com>
2017-12-23 16:18:44 +0700

    Add an image captioning model

--- ide/static/js/modelZoo.js
+++ ide/static/js/modelZoo.js
@@ -45,6 +45,8 @@ class ModelZoo extends React.Component {
           <div className="zoo-modal-model">
             <h3 className="zoo-modal-text">Caption</h3>
               <ModelElement importNet={this.props.importNet} framework="caffe" id="CoCo_Caption">CoCo Caption</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="keras" id="keras-image-caption">Keras Image Caption</ModelElement>
             <h3 className="zoo-modal-text">VQA</h3>
               <ModelElement importNet={this.props.importNet} framework="keras" id="VQA">VQA</ModelElement>
           </div>
--- tutorials/tested_models.md
+++ tutorials/tested_models.md
@@ -32,6 +32,8 @@ List of all models for which import/export has been tested with Fabrik.
 
 * COCO Caption [\[Source\]](https://github.com/jeffdonahue/caffe/tree/recurrent-rebase-cleanup/examples/coco_caption)[\[Visualise\]](http://fabrik.cloudcv.org/caffe/load?id=20171208113707zcgth)
 * VQA [\[Source\]](https://github.com/iamaaditya/VQA_Demo)
+* Keras Image Caption [\[Source\]](https://github.com/LemonATsu/Keras-Image-Caption)
+
 ## Miscellaneous
 
 * Ranking CNN [\[Source\]](https://github.com/RankingCNN/Using-Ranking-CNN-for-Age-Estimation)[\[Visualise\]](http://fabrik.cloudcv.org/caffe/load?id=20171208121544acjpu)
